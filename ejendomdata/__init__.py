6fd7145f3c5989d417706b415bb04abf624c124a
PyCartographer <146126638+NicklasKristiansen@users.noreply.github.com>
2025-07-18 09:35:19 +0000

    Started basic logging infrastructure

--- ejendomdata/__init__.py
+++ ejendomdata/__init__.py
@@ -1,4 +1,10 @@
 from __future__ import annotations
 from .constants import *
 from .api import *
-from .propertydata import * 
+from .propertydata import *
+from .setup_logging import _setup_logging
+_setup_logging(debug=False)
+
+def get_address(address: str) -> PropertyAddress:
+    address = search_for_address(address)
+    return PropertyAddress(address[0]["id"])
---
+++ ejendomdata/setup_logging.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+import logging
+import yaml
+
+
+def _setup_logging(debug=False):
+    with open("logging.yaml", "rt") as f:
+        config = yaml.safe_load(f.read())
+    
+    if debug:
+        config["loggers"]["mylib"]["level"] = "DEBUG"
+        config["handlers"]["console"]["level"] = "DEBUG"
+    else:
+        config["loggers"]["mylib"]["level"] = "INFO"
+        config["handlers"]["console"]["level"] = "INFO"
+    
+    logging.config.dictConfig(config)
+
