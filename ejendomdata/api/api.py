4adbf95e60031e8f8a11b35e946e9189b79d788b
PyCartographer <146126638+NicklasKristiansen@users.noreply.github.com>
2025-07-24 12:36:04 +0000

    updated api with status_code handler and constants with valid responses

--- ejendomdata/api/api.py
+++ ejendomdata/api/api.py
@@ -4,6 +4,7 @@ from functools import wraps
 import time
 import logging
 from ..constants import *
+from .status_code_handler import *
 import httpx
 
 logger = logging.getLogger(__name__)
@@ -16,31 +17,29 @@ __all__ = [
 ]
 
 
-# TODO: Handle different HTTP request responses especially 429
-
 
 def search_for_address(addresse: str) -> dict:
-    response = httpx.get(url=_DATAFORSYNING_URL_ + f"adresser?q={quote_plus(addresse)}").raise_for_status()
-    return response.json()
+    response = httpx.get(url=_DATAFORSYNING_URL_ + f"adresser?q={quote_plus(addresse)}")
+    return status_code_handler(response).json()
 
 
 def get_bfe(address_id: str) -> int:
-    response = httpx.get(url=_OIS_BASE_URL_ + "property/" + f"GetBFEFromAddressId?addressId={address_id}").raise_for_status()
-    return response.json()
+    response = httpx.get(url=_OIS_BASE_URL_ + "property/" + f"GetBFEFromAddressId?addressId={address_id}")
+    return status_code_handler(response).json()
 
 
 def get_address_history(bfe: str | int) -> dict:
     response = httpx.get(url=_OIS_BASE_URL_ + "svur/" + f"get?bfe={bfe}").raise_for_status()
-    return response.json()
+    return status_code_handler(response).json()
 
 def get_general_info(bfe: str | int) -> dict:
     response = httpx.get(url=_OIS_BASE_URL_ + "property/" + f"GetGeneralInfoFromBFE?bfe={bfe}").raise_for_status()
-    return response.json()
+    return status_code_handler(response).json()
 
 
 def get_sales_info(sales_id: int | str) -> dict:
     response = httpx.get(url=_OIS_BASE_URL_ + "svur/" + f"GetSalg?salgId={sales_id}").raise_for_status()
-    return response.json()
+    return status_code_handler(response).json()
     
 
     
--- ejendomdata/constants.py
+++ ejendomdata/constants.py
@@ -1,9 +1,13 @@
-import os
+from __future__ import annotations
 __all__ = [
     "_OIS_BASE_URL_",
     "_DAR_BFE_PUBLIC_URL_",
-    "_DATAFORSYNING_URL_"
+    "_DATAFORSYNING_URL_",
+    "VALID_STATUS_RESPONSES"
 ]
 _OIS_BASE_URL_ = "https://ois.dk/api/"
 _DATAFORSYNING_URL_ = "https://api.dataforsyningen.dk/"
 _DAR_BFE_PUBLIC_URL_ = "https://services.datafordeler.dk/DAR/DAR_BFE_Public/1/rest/"
+
+
+VALID_STATUS_RESPONSES = (200, 202, 204)
